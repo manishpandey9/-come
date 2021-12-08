6bbafedcc77cad5a57f432982916eb10406acdae
Manish <manish.p@healthify.co>
2021-12-08 23:57:00 +0530

    using helper function inside subtests

--- hello_test.go
+++ hello_test.go
@@ -3,27 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) { //it is accepting testing.TB which is an interface that *testing.T (it is for testing) and *testing.B (it is for benchmark)both satisfy
+		t.Helper() //it is needed to tell the test suite that this method is helper
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+			// https://golang.org/pkg/fmt/#hdr-Printing
+		}
+	}
 	t.Run("saying Hello to people", func(t *testing.T) {
 		got := Hello("Manish")
 		want := "Hello, Manish"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-			// https://golang.org/pkg/fmt/#hdr-Printing
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying 'Hello, World' when and empty string is supplied", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-			// https://golang.org/pkg/fmt/#hdr-Printing
-		}
+		want := "Hello, "
+		assertCorrectMessage(t, got, want)
+
 	})
 }
 
 /*INTRODCING Subtests
 the benefit of using subtests is we can use common shared data
 in many subtests
+*/
+
+/*IN go we can declare a function inside another function and assign
+it to a varible
 */
 
 /*Writing a test is just like writing a function, with a few rules
